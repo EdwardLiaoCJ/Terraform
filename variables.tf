bde33cbf73580f113d64665538b6487952b3d523
EdwardLiaoCJ <liaocj@cn.ibm.com>
2019-08-21 14:41:29 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,37 @@
-variable "datacenter" {
-  type        = "string"
-  description = "The data center that you want to deploy your Kubernetes cluster in."
-  default = "dal10"
+variable "org" {}
 
+variable "space" {}
+
+variable "region" {}
+
+variable "datacenter" {}
+
+variable "machine_type" {}
+
+variable "isolation" {}
+
+variable "private_vlan_id" {}
+
+variable "public_vlan_id" {}
+
+variable "subnet_id" {}
+
+variable "service_instance_name" {
+  default = "myservice"
+}
+
+variable "service_key" {
+  default = "myservicekey"
+}
+
+variable "service_offering" {
+  default = "speech_to_text"
+}
+
+variable "plan" {
+  default = "lite"
+}
+
+variable "cluster_name" {
+  default = "mycluster"
 }
