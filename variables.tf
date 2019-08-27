a2b6922839b2141a178f7eb91f4f208963a5910d
EdwardLiaoCJ <liaocj@cn.ibm.com>
2019-08-27 16:53:47 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,8 +4,12 @@ provider "ibm" {
   #softlayer_api_key  = "${var.ibm_sl_api_key}"
 }
 #variable ibm_bx_api_key {}
-#variable ibm_sl_username {}
-#variable ibm_sl_api_key {}
+variable ibm_sl_username {
+  default="liaocj@cn.ibm.com"
+  }
+variable ibm_sl_api_key {
+  deafult="ekIbsGMDdeCLnjvof1_7WkA5ik4-OMTC0bHuqk-yE3I0"
+  }
 data "ibm_space" "spacedata" {
   space = "dev"   # this will be different if you aren't is this space
   org   = "liaocj@cn.ibm.com" # this will be different if you aren't is this org
