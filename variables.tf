cb7db234dbe195cd5ed2eb39fc11bdfd47f92872
EdwardLiaoCJ <liaocj@cn.ibm.com>
2019-08-27 16:43:35 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 provider "ibm" {
-  bluemix_api_key    = "${var.ibm_bx_api_key}"
-  softlayer_username = "${var.ibm_sl_username}"
-  softlayer_api_key  = "${var.ibm_sl_api_key}"
+  #bluemix_api_key    = "${var.ibm_bx_api_key}"
+  #softlayer_username = "${var.ibm_sl_username}"
+  #softlayer_api_key  = "${var.ibm_sl_api_key}"
 }
-variable ibm_bx_api_key {}
-variable ibm_sl_username {}
-variable ibm_sl_api_key {}
+#variable ibm_bx_api_key {}
+#variable ibm_sl_username {}
+#variable ibm_sl_api_key {}
 data "ibm_space" "spacedata" {
   space = "dev"   # this will be different if you aren't is this space
   org   = "liaocj@cn.ibm.com" # this will be different if you aren't is this org
