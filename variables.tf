42cb84d5b2f63bb0d24e84231da165e3c2385acb
EdwardLiaoCJ <liaocj@cn.ibm.com>
2019-08-27 17:02:29 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,10 +5,10 @@ provider "ibm" {
 }
 #variable ibm_bx_api_key {}
 variable ibm_sl_username {
-  default="liaocj@cn.ibm.com"
+   
   }
 variable ibm_sl_api_key {
-  deafult="ekIbsGMDdeCLnjvof1_7WkA5ik4-OMTC0bHuqk-yE3I0"
+   
   }
 data "ibm_space" "spacedata" {
   space = "dev"   # this will be different if you aren't is this space
